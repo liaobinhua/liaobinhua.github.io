d19e2009f03d8e2a935b310a46529c6fef805d1a
BinhuaLiao <liaobinhua1994@gmail.com>
2018-12-23 01:05:58 +0800

    update

--- _fe/src/base.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-/**
-* here is base framework code for liaohquiu.net
-*/
-// load bootstrap
-require("bootstrap-sass!../bootstrap-sass.config.js");
-
-// style
-require('./style/blog.scss');
